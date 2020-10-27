e45d05a892c86c518361c7b0f1ebb9f2040e8a6d
Chris Palka <cpalka87gmail.com>
2020-10-26 20:08:19 -0600

    Install all core libraries, react, webpack, etc

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,14 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
+class App extends React.component {
+  render() {
+    return
+      <h1>My App</h1>
+  }
+}
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
