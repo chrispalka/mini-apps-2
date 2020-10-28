618fe7f44000d58efdae7a67588d9e7d7cb688ff
Chris Palka <cpalka87gmail.com>
2020-10-27 22:53:22 -0600

    Render entries on page in table format   -make GET request to json-server   -Limit 10 param

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Table from './components/Table.jsx';
+const axios = require('axios');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      data: [],
+    }
+    this.getHistoricalData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getHistoricalData()
+  }
+
+  getHistoricalData() {
+    const url = `http://localhost:3000/events?_page=1&_limit=10`
+    axios(url)
+      .then((response) => {
+        const { data } = response;
+        this.setState({ data: data })
+      })
+  }
+
+  handleSearch(e) {
+
+  }
   render() {
+    const { data } = this.state;
     return (
-      <h1>My App</h1>
+      <div>
+        <table style={{ width: '100%' }}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Language</th>
+              <th>Category 1</th>
+              <th>Category 2</th>
+              <th>Granularity</th>
+            </tr>
+          </thead>
+          <Table data={data} />
+        </table>
+      </div>
     )
   }
 }
