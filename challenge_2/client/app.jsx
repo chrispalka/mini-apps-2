19d1f6242ddd553ea918701c85463da49884732a
Chris Palka <cpalka87gmail.com>
2020-10-28 20:29:03 -0600

    Start server, serve webpack bundle via express, render component on page

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const axios = require('axios');
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <h1>hi</h1>
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+
+});
 
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Connected to server at: http://localhost:${PORT}`)
+});
